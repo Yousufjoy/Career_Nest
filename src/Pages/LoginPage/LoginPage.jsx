7948f7a04bcee259a8c47830aeb705b7aa3ec294
Yousufjoy <kanekiken333@gmail.com>
2023-11-29 17:07:20 +0600

    google signup complete

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,25 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProviders";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 const LoginPage = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const { loginUser } = useContext(AuthContext);
   const nav = useNavigate();
+  const auth = getAuth();
+  const loginWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        Navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -71,6 +85,31 @@ const LoginPage = () => {
               >
                 Login Now
               </button>
+              <div className="flex flex-col space-y-5">
+                <span className="flex items-center justify-center space-x-2">
+                  <span className="h-px bg-gray-400 w-14"></span>
+                  <span className="font-normal text-gray-500">
+                    or login with
+                  </span>
+                  <span className="h-px bg-gray-400 w-14"></span>
+                </span>
+                <div className="flex flex-col space-y-4">
+                  <a
+                    href="#"
+                    className="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-gray-800 rounded-md group hover:bg-gray-800 focus:outline-none"
+                  >
+                    <span>
+                      <AiFillGoogleCircle className=" text-blue-400 text-xl"></AiFillGoogleCircle>
+                    </span>
+                    <span
+                      onClick={loginWithGoogle}
+                      className="text-sm font-medium text-gray-800 group-hover:text-white"
+                    >
+                      Google
+                    </span>
+                  </a>
+                </div>
+              </div>
             </form>
             <p className=" bg-red-300 rounded-md my-3">{errorMessage}</p>
           </div>
